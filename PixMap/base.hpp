7bd0f1ca5bdcc67f2ade95e148aeb1121c69b950
ChrisZhang <zcyjim@outlook.com>
2021-02-24 22:47:23 +0800

    修正了gitignore文件，现在将不追踪所有ppm输出 修改了几个hpp文件的实现

--- PixMap/base.hpp
+++ PixMap/base.hpp
@@ -1,1 +1,2 @@
+#pragma once
 int round(double x) { return int(x + 0.5); }
--- PixMap/color.hpp
+++ PixMap/color.hpp
@@ -1,5 +1,9 @@
+#pragma once
 struct Color {
     unsigned char r;
     unsigned char g;
     unsigned char b;
+public:
+    Color(int red = 255, int green = 255, int blue = 255) :
+        r(red), g(green), b(blue) {}
 };

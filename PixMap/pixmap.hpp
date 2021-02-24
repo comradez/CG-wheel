c6f6deef7fd98c6ca840a28cd001ed329385f80e
ChrisZhang <zcyjim@outlook.com>
2021-02-24 21:33:50 +0800

    修改了pixmap，添加了drawpixel函数

--- PixMap/pixmap.hpp
+++ PixMap/pixmap.hpp
@@ -1,23 +1,27 @@
 #include <cstdio>
 #include <vector>
+#include <cassert>
 using std::vector;
+using RawMap = vector<vector<unsigned char>>;
+
+//TODO: 把assert改为try-catch
 
 class PixMap {
 private:
-    vector<vector<unsigned char>> _red;
-    vector<vector<unsigned char>> _green;
-    vector<vector<unsigned char>> _blue;
+    RawMap _red;
+    RawMap _green;
+    RawMap _blue;
     int _width, _height;
 public:
     PixMap(int width = 0, int height = 0)
         : _width(width), _height(height) {
-        _red.resize(height);
+        _red  .resize(height);
         _green.resize(height);
-        _blue.resize(height);
+        _blue .resize(height);
         for (int i = 0; i < height; i++) {
-            _red[i].resize(width);
+            _red[i]  .resize(width);
             _green[i].resize(width);
-            _blue[i].resize(width);
+            _blue[i] .resize(width);
         }
     }
     PixMap() : PixMap(0, 0) {}
@@ -25,12 +29,18 @@ public:
     int size() const {
         return _width * _height;
     }
-    void setMap(vector<vector<unsigned char>> red  ,
-                vector<vector<unsigned char>> green, 
-                vector<vector<unsigned char>> blue) {
+    void setMap(RawMap red, RawMap green, RawMap blue) {
         _red   = red;
         _green = green;
         _blue  = blue;
+    }
+    void drawPixel(int x, int y, int r, int g, int b) {
+        //以左上角为原点，向右为x轴正方向，向下为y轴正方向
+        assert(0 <= x && x < _width);
+        assert(0 <= y && y < _height);
+        _red[y][x]   = r;
+        _green[y][x] = g;
+        _blue[y][x]  = b;
     }
     void output(char* target) const {
         FILE* dst = fopen(target, "w");
